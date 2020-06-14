3470f01bd3a91a9596e9a3403fbe0d8a0ab6340b
Muhammad Qasim <41919676+qasim5611@users.noreply.github.com>
2020-06-14 16:17:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         heloo everyone Qasim Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          hellow how are you?          hellow how are you          hellow how are you          hellow how are you
+          hellow how are you?          hellow how are you          hellow how are you
         </p>
         <a
           className="App-link"
