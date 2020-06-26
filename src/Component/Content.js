d5954bccd2e914a28c19730430fa00a99dcb1110
Muhammad Qasim <41919676+qasim5611@users.noreply.github.com>
2020-06-26 20:57:19 +0500

    Update Content.js

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 // import { sendToActionFORRetrive } from '../Redux/Action/action';
 
 export class Welcom extends Component {
