7ee9f043ebb23b794142d435876865fdc27cd0c1
Muhammad Qasim <41919676+qasim5611@users.noreply.github.com>
2020-06-26 20:54:18 +0500

    Update Content.js

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
 // import { sendToActionFORRetrive } from '../Redux/Action/action';
 
 export class Welcom extends Component {
@@ -11,7 +12,22 @@ export class Welcom extends Component {
   render() {
     return (
       <div>
-        Welcom To Template
+        Welcom To Expense Tracker App By Qasim
+
+
+        <Container>
+        <Row>
+    {/*............................................Top Left Corner*/}
+    <Col xs={12} sm={12} lg={12} md={12}  style={{backgroundColor:'',height:"150px",boxShadow:"1px 1px 3px black"}}>
+   
+    <p style={{display:"inline",marginLeft:"20px", marginTop:"50px", fontFamily:"cursive", fontSize:"20px"}}>Qasim<br/>Join Date: 10-03-2019 </p>
+    
+    </Col>
+   
+   
+{/*............................................End of Top Right Corner*/}
+</Row>
+</Container>
       </div>
     )
   }
